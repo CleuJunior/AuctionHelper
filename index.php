bc65775c9f073c462035e9e94732fddd29740459
CleuJunior <cleu.junior@gmail.com>
2021-02-17 00:04:35 -0300

    Iniciando a paginas e criando os layouts

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include("bidder.php");
+include("item.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>AuctionHelper</title>
+    <link rel="stylesheet" type="text/css" href="ah_styles.css">
+</head>
+
+<body>
+    <header>
+        <?php include("header.inc.php"); ?>
+    </header>
+    <section id="container">
+        <nav>
+            <?php include("nav.inc.php"); ?>
+        </nav>
+        <main>
+            <?php
+                if (isset($_REQUEST['content'])) {
+                    include($_REQUEST['content'] . ".inc.php");
+
+                } else {
+                    include("main.inc.php");
+
+                }       
+            ?>
+        </main>
+        <aside>
+            <?php include("aside.inc.php"); ?>
+        </aside>
+    </section>
+    <footer>
+        <?php include("footer.inc.php"); ?>
+    </footer>
+</body>
+
+</html>
